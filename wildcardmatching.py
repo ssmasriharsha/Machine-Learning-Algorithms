2c19413434bdd6864400211ff32e59e7f94825f2
sriharsha-tech <72191737+sriharsha-tech@users.noreply.github.com>
2022-10-05 21:58:45 +0530

    Updated isMatch Function

--- wildcardmatching.py
+++ wildcardmatching.py
@@ -8,5 +8,18 @@ class Solution:
             if p1[-1] != '*' or p1[-1] == '*' and x != '*':
                 p1.append(x)
         return ''.join(p1)
+     def isMatch(self, s: str, p: str) -> bool:
+        p = self.removeDummyStars(p)
+        dp = [(len(p) + 1) * [False] for _ in range(len(s) + 1)]
+        dp[0][0] = True
+        if len(p) > 0 and p[0] == '*':
+            dp[0][1] = True
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                if s[i-1] == p[j-1] or p[j-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
+        return dp[len(s)][len(p)]
     
    
